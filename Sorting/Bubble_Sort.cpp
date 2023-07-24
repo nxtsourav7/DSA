36743ebb342df8e7e52917fb317c5d0e240506a1
nxtsourav7 <nxtsourav7@gmail.com>
2023-07-24 11:17:16 +0600

    Added Bubble_Sort

---
+++ Sorting/Bubble_Sort.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void Bubble_Sort(int array[] , int n) {
+    for(int i=0 ; i<n-1 ; i++) {
+
+        bool flag = false;
+
+        for(int j=0 ; j<n-i-1 ; j++) {
+            if(array[j]>array[j+1]) {
+                swap(array[j] , array[j+1]);
+                flag = true;
+            }
+        }
+
+        if (!flag) break;
+    }
+}
+
+
+void print(int array[] , int n) {
+    for(int i=0 ; i<n ; i++) {
+        cout << array[i] << " ";
+    }
+    cout << '\n';
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    int array[n];
+    for(auto &a : array) cin >> a;
+
+    Bubble_Sort(array , n);
+
+    cout << "Sorted Array : \n";
+    print(array , n);
+    return 0;
+}
