10fba40b948a7e66ec558e135f9b264e985ae37b
nxtsourav7 <nxtsourav7@gmail.com>
2023-07-30 22:30:37 +0600

    added Linear_Search

---
+++ Searching/Linear_Search.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int Linear_Search(int array[] , int n , int x) {
+    for(int i=0 ; i<n ; i++){
+        if(array[i]==x) return i;
+    }return -1;
+}
+
+
+int main() {
+    int n;cin >>n;
+
+    int array[n];
+    for(auto &a : array) cin >> a;
+
+    // cout << "Enter Searching Element : ";
+    int x;cin >> x;
+
+    int ans = Linear_Search(array , n , x);
+    if(ans != -1) {
+        cout << "Found !\n";
+        cout << "Position : " << ans+1 << '\n';
+    }
+    else {
+        cout << "Not Found !\n";
+    }
+
+    return 0;
+}
