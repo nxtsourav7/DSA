32a5bfe750cef26d502670c2885e1e53e4fa36a8
nxtsourav7 <nxtsourav7@gmail.com>
2023-10-17 22:33:18 +0600

    change

--- Structure/Find_Maximum_2D.cpp
+++ Structure/Find_Maximum_2D.cpp
@@ -6,39 +6,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//int a[100][100];
+int a[100][100];
 
-struct output{
+struct number{
     int value;
+	int position_row;
     int position_column;
-    int position_row;
 };
 
 
-output mx (int a[][7],int row , int col ) {
-    int maximum = a[0][0];
-	int pos_max_row = 0 , pos_max_col = 0;
+number maximum_number(int row , int col ) {
+	number maximum;
+    maximum.value = a[0][0];
+	maximum.position_row = 0 , maximum.position_column = 0;
 	for(int i=0 ; i<row ; ++i) {
 		for(int j=0 ; j<col ; ++j) {
 			// find maximum
-			if(a[i][j] > maximum) {
-				maximum = a[i][j];
-				pos_max_row = i;
-				pos_max_col = j;
+			if(a[i][j] > maximum.value) {
+				maximum.value = a[i][j];
+				maximum.position_row = i;
+				maximum.position_column = j;
 			}
 		}
 	}
 
-
-    output x;
-    x.value = maximum;
-    x.position_row = pos_max_row;
-    x.position_column = pos_max_col;
-
-    return x;
+    return maximum;
 }
-
-
 
 int main(void) {
     ios_base::sync_with_stdio(0);
@@ -55,13 +48,11 @@ int main(void) {
 			cin >> a[i][j];
 		}
 	}
-    output x = mx(a,row , col);
+    number maximum = maximum_number(row , col);
 	
-	// Print min and max with position
-	// cout << "Minimum : " << minimum << "\n";
-	// cout << "Position : [" << pos_min_row << " " << pos_min_col << "]\n";
-	cout << "Maximum : " << x.value << "\n";
-	cout << "Position : [" << x.position_row << " " << x.position_column << "]\n";
+	// Print Maximum with position
+	cout << "Maximum : " << maximum.value << "\n";
+	cout << "Position : [" << maximum.position_row << " " << maximum.position_column << "]\n";
 	
     return 0;
 }
